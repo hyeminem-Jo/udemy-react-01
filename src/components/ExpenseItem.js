5a7a49d01c33837defaafe3f1a9084c09ce39fa5
hyeminem-Jo <konnimey@naver.com>
2023-05-31 14:58:13 +0900

    📖 Course 45 (23.05.31)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,18 @@
+import React from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 const ExpenseItem = ({ title, amount, date }) => {
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">{amount}</div>
       </div>
-    </div>
+    </Card>
   );
 };
 
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,15 +1,20 @@
+import React from 'react';
+// 리액트 예전 버전에서는 위와 같이 페이지마다 react 를 import 해야했지만 이제 그럴 필요 없어짐
+// 기술적으로는 필요없어졌지만 명시적으로, jsx 코드에서 리액트가 내부적으로 사용되고 있음을 알리기 위해 표시해준다.
+
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
+import Card from "./Card";
 
 const Expenses = ({ items }) => {
 
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem title={items[0].title} amount={items[0].amount} date={items[0].date} />
       <ExpenseItem title={items[1].title} amount={items[1].amount} date={items[1].date} />
       <ExpenseItem title={items[2].title} amount={items[2].amount} date={items[2].date} />
       <ExpenseItem title={items[3].title} amount={items[3].amount} date={items[3].date} />
-    </div>
+    </Card>
   );
 };
 
