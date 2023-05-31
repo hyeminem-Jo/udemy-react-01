e95041df9c82ee6c40709147c888de0295ee34e7
hyeminem-Jo <konnimey@naver.com>
2023-05-31 17:06:45 +0900

    📖 Course 52 (23.05.31)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
-import Card from "./Card";
+import Card from "../UI/Card";
 
 const Expenses = ({ items }) => {
 
